d3de7986e4cce68194baf7ffbfd5697560f2850c
Jeswin2711 <jeswin2711@gmail.com>
2022-07-18 17:49:53 +0530

    Added:UseCase3 & Refactored:Comments

--- src/main/java/com/linecomparision/LineCalculation.java
+++ src/main/java/com/linecomparision/LineCalculation.java
@@ -4,21 +4,19 @@ class LineCalculation
 {
     private double x,y;
 
+    /*
+    A Constructor to assign two coordinates to x and y
+     */
     LineCalculation(double x1, double y1) {
         x = x1;
         y = y1;
     }
+    /*
+    Function Distance with Return type Double is used find the distance of two points
+     */
     double distance(LineCalculation p)
     {
         return Math.sqrt( Math.pow(p.x - x,2) + Math.pow(p.y -y,2) );
     }
 }
 
-
-
-/*
-LineCalculation point1_line1= new LineCalculation(2,1);
-        LineCalculation point2_line1= new LineCalculation(8,2);
-        double line1 = point1_line1.distance(point2_line1);
-        System.out.println("Length of First Line : " + line1);
-// */
