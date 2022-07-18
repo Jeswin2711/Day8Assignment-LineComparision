3dfac709d006f9a4ff7ddb3cfb73c92d66c6542b
Jeswin <99168597+Jeswin2711@users.noreply.github.com>
2022-07-18 17:51:22 +0530

    Refactored

--- src/main/java/com/linecomparision/LineCalculation.java
+++ src/main/java/com/linecomparision/LineCalculation.java
@@ -13,12 +13,3 @@ class LineCalculation
         return Math.sqrt( Math.pow(p.x - x,2) + Math.pow(p.y -y,2) );
     }
 }
-
-
-
-/*
-LineCalculation point1_line1= new LineCalculation(2,1);
-        LineCalculation point2_line1= new LineCalculation(8,2);
-        double line1 = point1_line1.distance(point2_line1);
-        System.out.println("Length of First Line : " + line1);
-// */
