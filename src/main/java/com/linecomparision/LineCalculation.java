89ad6e8dcc72b6610aeac400f9ad8904047a4107
Jeswin <99168597+Jeswin2711@users.noreply.github.com>
2022-07-18 17:51:52 +0530

    Refactored:Comments

--- src/main/java/com/linecomparision/LineCalculation.java
+++ src/main/java/com/linecomparision/LineCalculation.java
@@ -8,6 +8,7 @@ class LineCalculation
         x = x1;
         y = y1;
     }
+    
     double distance(LineCalculation p)
     {
         return Math.sqrt( Math.pow(p.x - x,2) + Math.pow(p.y -y,2) );
