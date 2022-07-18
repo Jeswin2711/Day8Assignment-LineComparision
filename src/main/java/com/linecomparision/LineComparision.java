bd8e7f03ef225c03c0f065513d906b78383d11aa
Jeswin2711 <jeswin2711@gmail.com>
2022-07-18 17:33:57 +0530

    Added:UseCase2

--- src/main/java/com/linecomparision/LineComparision.java
+++ src/main/java/com/linecomparision/LineComparision.java
@@ -9,5 +9,10 @@ public class LineComparision
         LineCalculation point2_line1= new LineCalculation(8,2);
         double line1 = point1_line1.distance(point2_line1);
         System.out.println("Length of First Line : " + line1);
+        LineCalculation point1_line2= new LineCalculation(2,0);
+        LineCalculation point2_line2= new LineCalculation(5,1);
+        double line2 = point1_line2.distance(point2_line2);
+        System.out.println("Length of Second Line : " + line2);
+        CheckLength checkLength = new CheckLength(line1,line2);
     }
 }
